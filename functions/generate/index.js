8f6f75bbda644eb8212ac63afab2bd7a72866d4c
steam0276430 <steam0276430@gmail.com>
2026-02-15 20:02:16 +0100

    Update index.js

--- functions/generate/index.js
+++ functions/generate/index.js
@@ -1,1 +1,8 @@
+export async function onRequestGet(context) {
+  return new Response("route generate OK");
+}
+
+export async function onRequestPost(context) {
+  return new Response("POST OK");
+}
 
